a261223b459cbe023acc0df79bf1a8fa8c1db975
Océane <119302835+Oceane-lemarinel@users.noreply.github.com>
2024-12-06 13:48:12 +0100

    main parameters fix

--- App/main_CLI.cpp
+++ App/main_CLI.cpp
@@ -8,7 +8,6 @@
 
 using namespace std;
 
-
 bool validateFile(const std::string& filename) {
     std::ifstream file(filename);
     return file.good();
@@ -19,9 +18,10 @@ int main(int argc, char *argv[])
     int opt;
     string inputPath;
     string outputPath;
-    int maxIterations;
+    int maxIterations = 0; // Initialize to 0
+    bool verboseMode = false;
 
-    while ((opt = getopt(argc, argv, "s:vf:")) != -1)
+    while ((opt = getopt(argc, argv, "p:vo:i:h")) != -1)
     {
         switch (opt)
         {
@@ -30,10 +30,11 @@ int main(int argc, char *argv[])
             break;
 
         case 'v':
+            verboseMode = true;
             cout << "Adaptive Horizon \n"
-            << "Made by: POLETTE Nathan & LEMARINEL Océane \n"
-            <<"Last update: 06/12/2024 \n"
-            <<"Version 0.28 \n";
+                 << "Made by: POLETTE Nathan & LEMARINEL Océane \n"
+                 << "Last update: 06/12/2024 \n"
+                 << "Version 0.28 \n";
             break;
 
         case 'o':
@@ -41,35 +42,38 @@ int main(int argc, char *argv[])
             break;
 
         case 'i':
-            maxIterations = atoi(optarg);
+            maxIterations = std::stoi(optarg);
             break;
 
         case 'h':
             cout << "Adaptive Horizon \n"
-            <<"\n"
-            <<"\n"
-            <<"-help -h     Displays help information\n\n"
-            <<"-v           Displays game version and extra info about it\n\n"
-            <<"-p path      Gives input file path for execution [mandatory]\n\n"
-            <<"-o name      Gives folder name for iteration outputs\n\n"
-            <<"-i number    Gives max iteration amount\n\n";
+                 <<"\n"
+                 <<"\n"
+                 <<"-help -h     Displays help information\n\n"
+                 <<"-v           Displays game version and extra info about it\n\n"
+                 <<"-p path      Gives input file path for execution [mandatory]\n\n"
+                 <<"-o name      Gives folder name for iteration outputs\n\n"
+                 <<"-i number    Gives max iteration amount\n\n";
             return 0;
-        default :
-            break;
+
+        default:
+            cerr << "Invalid option. Use -h for help." << endl;
+            return 1;
         }
     }
-    
-    while(!validateFile(inputPath)){
-        cout << "input path not provided / can't find file \n please provide file name / path";
-        cin >> inputPath ;
+
+    while (inputPath.empty() || !validateFile(inputPath)) {
+        cout << "Input path not provided or file not found. Please provide a valid file name/path: ";
+        std::getline(cin, inputPath);
     }
 
-    //
-    // Finally running everything according to parameters
-    //
+    if (verboseMode) {
+        cout << "Input Path: " << inputPath << endl;
+        cout << "Output Path: " << (outputPath.empty() ? "Not specified" : outputPath) << endl;
+        cout << "Max Iterations: " << (maxIterations > 0 ? std::to_string(maxIterations) : "Unlimited") << endl;
+    }
 
     GameOfLifeCLI game(inputPath, outputPath, maxIterations);
-    int status = game.run();
-    assert(status == 1 || status == 2);
+    game.run();
     return 0;
 }
